d902d188434b5ff2b5b7f888c5e027d008363137
Lokesh <lokeshmaddila1982@gmail.com>
2026-01-31 08:32:53 +0530

    Add pluggable shuffle strategy support to Deck
    Introduced the IShuffleStrategy interface and a FisherYatesShuffleStrategy implementation to enable flexible shuffling algorithms for Deck. Refactored Deck to support strategy-based shuffling and deprecated the original Shuffle(int times) implementation. This enhances extensibility and testability of deck shuffling.

--- CSharp/Blackbaud.Interview.Cards/Deck.cs
+++ CSharp/Blackbaud.Interview.Cards/Deck.cs
@@ -1,6 +1,49 @@
 namespace Blackbaud.Interview.Cards;
 
 /// <summary>
+/// Strategy for shuffling a list of cards
+/// </summary>
+public interface IShuffleStrategy
+{
+    /// <summary>
+    /// Shuffle the provided list of cards in-place.
+    /// </summary>
+    /// <param name="cards">List of cards to shuffle</param>
+    void Shuffle(List<Card> cards);
+}
+
+/// <summary>
+/// Fisher-Yates shuffle strategy. Performs the shuffle the specified number of times.
+/// </summary>
+public class FisherYatesShuffleStrategy : IShuffleStrategy
+{
+    private readonly int _times;
+    private readonly Random _rng;
+
+    public FisherYatesShuffleStrategy(int times = 1, Random? rng = null)
+    {
+        if (times < 1) throw new ArgumentOutOfRangeException(nameof(times));
+        _times = times;
+        _rng = rng ?? Random.Shared;
+    }
+
+    public void Shuffle(List<Card> cards)
+    {
+        if (cards == null) throw new ArgumentNullException(nameof(cards));
+        for (int t = 0; t < _times; t++)
+        {
+            for (int i = cards.Count - 1; i > 0; i--)
+            {
+                int j = _rng.Next(i + 1);
+                var tmp = cards[i];
+                cards[i] = cards[j];
+                cards[j] = tmp;
+            }
+        }
+    }
+}
+
+/// <summary>
 /// A deck of cards
 /// </summary>
 public class Deck
@@ -59,6 +102,37 @@ public class Deck
     }
 
     /// <summary>
+    /// Shuffles the deck the specified number of times using Fisher-Yates shuffle.
+    /// This overload is kept for convenience and delegates to the strategy-based shuffle.
+    /// </summary>
+    /// <param name="times">Number of shuffle passes to perform; must be >= 1.</param>
+    public void Shuffle(int times = 1)
+    {
+        Shuffle(new FisherYatesShuffleStrategy(times));
+    }
+
+    /// <summary>
+    /// Shuffles the deck using the provided shuffle strategy.
+    /// </summary>
+    /// <param name="strategy">Strategy to perform the shuffle</param>
+    public void Shuffle(IShuffleStrategy strategy)
+    {
+        if (strategy == null) throw new ArgumentNullException(nameof(strategy));
+
+        // Copy current cards into a list (enumeration of Stack is LIFO: top first)
+        var list = _stackOfCards.ToList();
+
+        strategy.Shuffle(list);
+
+        // Rebuild the stack so that the first element of the list becomes the next popped card
+        _stackOfCards.Clear();
+        for (int i = list.Count - 1; i >= 0; i--)
+        {
+            _stackOfCards.Push(list[i]);
+        }
+    }
+
+    /// <summary>
     /// Randomizes the order of the cards in the deck using the Fisher-Yates algorithm.
     /// </summary>
     /// <param name="times">The number of times to perform the shuffle pass. Must be &gt;= 1. Default is 1.</param>
@@ -73,6 +147,7 @@ public class Deck
     ///   card returned by <see cref="NextCard"/>.
     /// Uses <see cref="Random.Shared"/> for a thread-safe random number generator.
     /// </remarks>
+    /*
     public void Shuffle(int times = 1)
     {
         if (times < 1)
@@ -102,4 +177,5 @@ public class Deck
             _stackOfCards.Push(lstCards[i]);
         }
     }
+    */
 }
